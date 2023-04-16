4dced9c1ff05e78b556bfbba9c8e688abae2a397
OlegsJanuskevics221RDC059 <124894319+OlegsJanuskevics221RDC059@users.noreply.github.com>
2023-04-16 19:49:58 +0000

    Upgraded code Oļegs Januškevičs 221RDC059

--- hash_substring.py
+++ hash_substring.py
@@ -2,8 +2,8 @@
 
 def read_input():
     pattern = input().rstrip()
-    text = input().rstrip() 
-    return pattern,text
+    text = input().rstrip()
+    return pattern, text
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
@@ -13,24 +13,30 @@ def get_occurrences(pattern, text):
     p_len = len(pattern)
     t_len = len(text)
 
+    # Calculate the hash of the pattern
     p_hash = 0
     for i in range(p_len):
-        p_hash = p_hash + ord(pattern[i]) * pow(10,p_len-i-1)
+        p_hash += ord(pattern[i]) * pow(10, p_len-i-1)
 
+    # Calculate the hash of the initial substring of length p_len in the text
     t_hash = 0
     for i in range(p_len):
-        t_hash = t_hash + ord(text[i]) * pow(10,p_len-i-1)
+        t_hash += ord(text[i]) * pow(10, p_len-i-1)
+
     for i in range(t_len - p_len + 1):
+        # Check if the hashes match
         if p_hash == t_hash:
+            # Check if the substrings match
             if pattern == text[i:i+p_len]:
                 positions.append(i)
+        
+        # Recalculate the hash for the next substring in the text
         if i < t_len - p_len:
-            t_hash = (t_hash - ord(text[i]) * pow(10,p_len-1)) *10 +ord(text[i+p_len])
-            return positions
+            t_hash = (t_hash - ord(text[i]) * pow(10, p_len-1)) * 10 + ord(text[i+p_len])
 
+    return positions
 
-if __name__ == '__main__':
-    pattern,text = read_input()
-    occurrences = get_occurrences(pattern,text)
+if _name_ == '_main_':
+    pattern, text = read_input()
+    occurrences = get_occurrences(pattern, text)
     print_occurrences(occurrences)
-
