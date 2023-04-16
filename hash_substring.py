9772a6fbfe4d36d359bd8915692b5d611eeca668
OlegsJanuskevics221RDC059 <124894319+OlegsJanuskevics221RDC059@users.noreply.github.com>
2023-04-16 21:47:30 +0000

    Upgraded code Oļegs Januškevičs 221RDC059

--- hash_substring.py
+++ hash_substring.py
@@ -15,24 +15,19 @@ def get_occurrences(pattern, text):
     p_len = len(pattern)
     t_len = len(text)
 
-    # Calculate the hash of the pattern
     p_hash = 0
     for i in range(p_len):
         p_hash += ord(pattern[i]) * pow(10, p_len-i-1)
 
-    # Calculate the hash of the initial substring of length p_len in the text
     t_hash = 0
     for i in range(p_len):
         t_hash += ord(text[i]) * pow(10, p_len-i-1)
 
     for i in range(t_len - p_len + 1):
-        # Check if the hashes match
         if p_hash == t_hash:
-            # Check if the substrings match
             if pattern == text[i:i+p_len]:
                 positions.append(i)
         
-        # Recalculate the hash for the next substring in the text
         if i < t_len - p_len:
             t_hash = (t_hash - ord(text[i]) * pow(10, p_len-1)) * 10 + ord(text[i+p_len])
 
