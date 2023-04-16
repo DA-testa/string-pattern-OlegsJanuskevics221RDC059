1e7ac56a302aedcf8abea6e40426bf3b8709e6c4
OlegsJanuskevics221RDC059 <124894319+OlegsJanuskevics221RDC059@users.noreply.github.com>
2023-04-16 21:09:53 +0000

    Upgraded code Oļegs Januškevičs 221RDC059

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,55 @@
 # python3
 
-import sys
+def read_input():
+    pattern = input().rstrip()
+    text = input().rstrip()
+    return pattern, text
 
-def rabin_karp(pattern, text):
+def print_occurrences(output):
+    print(' '.join(map(str, output)))
+
+def get_occurrences(pattern, text):
+    positions = []
     p_len = len(pattern)
     t_len = len(text)
-    p_hash = hash(pattern)
-    t_hashes = [hash(text[i:i+p_len]) for i in range(t_len-p_len+1)]
-    res = []
-    for i in range(t_len-p_len+1):
-        if p_hash == t_hashes[i]:
-            if text[i:i+p_len] == pattern:
-                res.append(i)
-    return res
 
-def main():
-    input_choice = input().strip()
-    if input_choice == "F":
-        filename = input().strip()
-        with open(filename) as f:
-            pattern = f.readline().strip()
-            text = f.readline().strip()
-    else:
-        pattern = input().strip()
-        text = input().strip()
-    res = rabin_karp(pattern, text)
-    for i in res:
-        print(i, end=" ")
+    # Calculate the hash of the pattern
+    p_hash = 0
+    for i in range(p_len):
+        p_hash += ord(pattern[i]) * pow(10, p_len-i-1)
 
-if __name__ == "__main__":
-    main()
+    # Calculate the hash of the initial substring of length p_len in the text
+    t_hash = 0
+    for i in range(p_len):
+        t_hash += ord(text[i]) * pow(10, p_len-i-1)
+
+    for i in range(t_len - p_len + 1):
+        # Check if the hashes match
+        if p_hash == t_hash:
+            # Check if the substrings match
+            if pattern == text[i:i+p_len]:
+                positions.append(i)
+        
+        # Recalculate the hash for the next substring in the text
+        if i < t_len - p_len:
+            t_hash = (t_hash - ord(text[i]) * pow(10, p_len-1)) * 10 + ord(text[i+p_len])
+
+    return positions
+
+if _name_ == '_main_':
+    text_type = "I"  # or "F", depending on the input method you want to use
+    if "F" in text_type:
+        filename = "06"
+        file_path = f"./test/{filename}"
+        try:
+            with open(file_path) as f:
+                pattern = f.readline().rstrip()
+                text = f.readline().rstrip()
+        except Exception as e:
+            print("Error:", str(e))
+            sys.exit()
+    elif "I" in text_type:
+        pattern, text = read_input()
+    
+    occurrences = get_occurrences(pattern, text)
+    print_occurrences(occurrences)
