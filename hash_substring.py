d52e1a5c724dc2bd8f8b536e7e63ebd82f6f8f2a
OlegsJanuskevics221RDC059 <124894319+OlegsJanuskevics221RDC059@users.noreply.github.com>
2023-04-16 20:11:17 +0000

    Upgraded code Oļegs Januškevičs 221RDC059

--- hash_substring.py
+++ hash_substring.py
@@ -36,7 +36,7 @@ def get_occurrences(pattern, text):
 
     return positions
 
-if _name_ == '__main__':
+if __name__ == '_main_':
     pattern, text = read_input()
     occurrences = get_occurrences(pattern, text)
     print_occurrences(occurrences)
