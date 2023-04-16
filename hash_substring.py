c45fd84a68e653282fe762bad4f532530812faa4
OlegsJanuskevics221RDC059 <124894319+OlegsJanuskevics221RDC059@users.noreply.github.com>
2023-04-16 19:31:29 +0000

    Upgraded code Oļegs Januškevičs 221RDC059

--- hash_substring.py
+++ hash_substring.py
@@ -3,7 +3,8 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    pattern = input().rstrip()
+    text = input().rstrip() 
     
     # after input type choice
     # read two lines 
@@ -21,12 +22,28 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    positions = []
+    p_len = len(pattern)
+    t_len = len(text)
 
+    p_hash = 0
+    for in range(p_len):
+        p_hash = p_hash + ord(pattern[i]) * pow(10,p_len-i-1)
+
+    t_hash = 0
+    for i in range(t_len - p_len + 1):
+        if p_hash == t_hash:
+            if pattern == text[i:i+p_len]:
+                positions.append(i)
+                if i < t_len - p_len:
+                    t_hash = (t_hash - ord(text[i]) * pow(10,p_len-1)) *10 +ord(text[i+p_len])
     # and return an iterable variable
-    return [0]
+    return positions
 
 
 # this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    pattern,text = read_input()
+    occurrences = get_occurrences(pattern,text)
+    print_occurrences(occurrences)
 
